7af79b2d93805faae35d0ad5b4c8b2acf72f6919
agus <agustinadaro@hotmail.com>
2022-01-10 21:19:44 -0300

    first commit

---
+++ bar.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Move this file wherever it's configured in the autostart in config.h
+
+	status(){
+
+		# Temperature:
+		cpu_temp=$(sed 's/000$//' /sys/class/thermal/thermal_zone7/temp)
+		#cpu_temp=$(sensors coretemp-isa-0000 | grep Package)
+		#cpu_temp=${cpu_temp:16:2}
+		if [ $cpu_temp -gt 70 ]; then
+			cpu_temp=" $cpu_temp";
+		else
+			cpu_temp=" $cpu_temp";
+		fi
+
+		# Battery		
+		capacity=$(cat /sys/class/power_supply/BAT0/capacity) 
+		if [ $capacity -gt 90 ]; then
+			capacity="  $capacity"	
+		elif [ $capacity -lt 30 ]; then
+			capacity="  $capacity"	
+		else
+			capacity="  $capacity"	
+		fi
+
+		status=$(cat /sys/class/power_supply/BAT0/status)
+		if [ $status = 'Discharging' ]; then
+			status="  "
+		elif [ $status = 'Full' ]; then
+			status="  "
+		elif [ $status = 'Charging' ]; then
+			status="  "
+		fi
+ 
+
+		# Backlight
+		brightness=" $(( `cat /sys/class/backlight/intel_backlight/brightness` * 100 / `cat /sys/class/backlight/intel_backlight/max_brightness` ))";
+
+
+		# Date and time
+		date="  $( date '+%a %d/%m %H:%M' )";
+
+		# Volume
+		is_mute=$(pactl get-sink-mute @DEFAULT_SINK@)
+		if [ "$is_mute" = "Mute: yes" ]; then 
+			is_mute=1; 
+		else
+			is_mute=0;
+		fi
+
+		long_volume=$(pactl get-sink-volume @DEFAULT_SINK@);
+		
+		if [ $is_mute = 1 ]; then
+			short_volume="  0";
+		else
+			short_volume=" ${long_volume:28:4}";
+		fi
+
+		# Keyboard layout
+		layout_num=$(xset -q|grep LED| awk '{ print $10 }')
+		kb_layout=" "
+
+		if [ $layout_num -lt 10 ]; then
+			kb_layout="$kb_layout us"
+		elif [ $layout_num -lt 1010 ]; then
+			kb_layout="$kb_layout es"
+		fi
+
+		
+		echo "$short_volume | $brightness | $capacity - $status | $kb_layout | $cpu_temp | $date";
+
+	}
+
+while true; do
+
+	xsetroot -name "$(status)"
+
+	sleep 20
+done
--- config.h
+++ config.h
@@ -1,5 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+#include "fibonacci.c"
+
+//static const char *upvol[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL };
+//static const char *downvol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",     NULL };
+//static const char *mutevol[] = { "pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle",  NULL };
+//static const char *brightness[] ={"brightnessctl", "set", "6000+", NULL};
+//static const char *brightness2[]= {"brightnessctl", "set", "6000-", "--min-value=1", "NULL"};
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -9,22 +17,33 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "Font Awesome 5 Brands:size=15" };
+static const char dmenufont[]       = "Font Awesome 5 Brands:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_black[]       = "#000000";
+static const char col_dark_purple[] = "#6A1A87";
+static const char col_white[] 	    = "#FFFFFF";
+static const char col_purple[] 	    = "#ab20fd";
 static const char col_cyan[]        = "#005577";
+static const char col_pink[]        = "#f000ff";
+static const char col_aqua[]   	    = "#76FFFD";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_aqua, col_black, col_gray2 },
+	[SchemeSel]  = { col_white, col_dark_purple,  col_aqua },
 };
 
 static const char *const autostart[] = {
-	"st", NULL,
+	"nm-applet", NULL,
+	"sh", "-c", "numlockx &", NULL,
+	"/home/agus/.fehbg", NULL,
+	"/usr/local/bin/bar.sh", NULL,
+	"sh", "-c", "setxkbmap us,es -option 'grp:alt_shift_toggle'", NULL,
 	NULL /* terminate */
 };
 
@@ -47,18 +66,17 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
-#include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "[\\]",      dwindle },
+	{ "[M]",      monocle },
+	{ "[@]",      spiral },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
- 	{ "[@]",      spiral },
- 	{ "[\\]",      dwindle },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -70,13 +88,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
+static const char *screenshot[]  = { "gnome-screenshot", "-i", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,            		XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,		XK_s,	   spawn,	   {.v = screenshot } },	
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -84,7 +105,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,     {0} },
+	{ MODKEY|ShiftMask,		XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -92,15 +114,20 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY,                       XK_space,  setlayout, 	   {0} },
+	{ Mod1Mask,			XK_Shift_L, spawn,	   SHCMD("slp=$(pidof sleep 20); kill $slp") },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ 0,                       XF86XK_AudioMute, spawn, SHCMD("pactl set-sink-mute @DEFAULT_SINK@  toggle;slp=$(pidof sleep 20); kill $slp") },
+	{ 0,                       XF86XK_AudioLowerVolume, 	 spawn, SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -3%;slp=$(pidof sleep 20); kill $slp")},
+	{ 0,                       XF86XK_AudioRaiseVolume, spawn, SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +3%;slp=$(pidof sleep 20); kill $slp") },
+  	{ 0,        		XF86XK_MonBrightnessUp,  spawn, SHCMD("brightnessctl set 6000+;slp=$(pidof sleep 20); kill $slp") },
+	{ 0,       		XF86XK_MonBrightnessDown,spawn, SHCMD("brightnessctl set --min-value=1 6000-;slp=$(pidof sleep 20); kill $slp") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -110,8 +137,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-};
+	{MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	};
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
@@ -129,3 +156,4 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
