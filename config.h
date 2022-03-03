1903ad8084e24d9f791253c7827bf9c3978e780b
agus <agustinadaro@hotmail.com>
2022-03-03 11:41:30 -0300

    fixed layouts order

--- config.h
+++ config.h
@@ -64,17 +64,17 @@ static const Rule rules[] = {
 
 
 /* layout(s) */
-static const float mfact     = 0.62; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.60; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[\\]",      dwindle },
+	{ "[]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
 	{ "[@]",      spiral },
-	{ "[]=",      tile },    /* first entry is default */
+	{ "[\\]",      dwindle },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
